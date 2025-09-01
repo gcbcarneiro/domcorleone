26328a48ada04105e78ee1de7aa26100e4989b3a
Gabriel Carneiro <38336597+gcbcarneiro@users.noreply.github.com>
2025-09-01 11:59:35 -0300

    Update settings.py

--- domcorleone/settings.py
+++ domcorleone/settings.py
@@ -58,12 +58,13 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            '/root/domcorleone/templates',  # caminho absoluto
+            '/root/domcorleone/templates',  # Caminho absoluto
+            os.path.join(BASE_DIR, 'templates'),  # Caminho relativo (mantém os dois)
         ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
+                'django.template.context_processors.debug',  # Adicione esta linha
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
